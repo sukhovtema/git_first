a241585bf5db8eee90cee519f5425bb4ffc6072b
Artem Sukhov <sukhovtema@gmail.com>
2022-10-28 16:17:28 +0300

    Добавьте файлы проекта.

---
+++ git_first/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace git_first
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 55, 8, -41, 2, 4, 2, 89, 33, -5, 12 };
+
+            int temp;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("Вывод отсортированного массива");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+        }
+    }
+}
