9812f8ecdae8d308ff95c3bcbd826bb05e240d8c
Alexsa7161 <alexsa7161@gmail.com>
2022-10-28 17:10:00 +0300

    Лёша

--- git_first/Program.cs
+++ git_first/Program.cs
@@ -28,5 +28,28 @@ namespace git_first
                 Console.WriteLine(nums[i]);
             }
         }
+        // Сортировка вставками
+        static void Swap(int[] array, int i, int j)
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        static void InsertionSort(int[] inArray)
+        {
+            int x;
+            int j;
+            for (int i = 1; i < inArray.Length; i++)
+            {
+                x = inArray[i];
+                j = i;
+                while (j > 0 && inArray[j - 1] > x)
+                {
+                    Swap(inArray, j, j - 1);
+                    j -= 1;
+                }
+                inArray[j] = x;
+            }
+        }
     }
 }
